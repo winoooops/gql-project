72fe29cba81f420bedc2e75639b4ba61c9da38fe
winoooops <winoooops@outlook.com>
2021-10-09 11:52:28 +0800

    feature: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
+  // 生产app
   const app = await NestFactory.create(AppModule);
+
+  // swagger文档
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('Vue-Tracker-Backend')
+    .setDescription('轻量化项目进度跟踪_后端')
+    .setVersion('0.1')
+    .addBearerAuth() //鉴权
+    .build()
+
+  const document = SwaggerModule.createDocument(app, swaggerOptions)
+  SwaggerModule.setup('doc', app, document) // doc -> swagger服务访问的路径
+
   await app.listen(4000);
 }
 bootstrap();
